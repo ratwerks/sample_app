82d7a4f9d225d1f9bdd36ce832d8492b79daa9c3
James B Robinson <jr@ratwerks.com>
2014-06-24 13:17:41 -0700

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # RoR Tut jr 20140624
+  config.include Capybara::DSL
 end
